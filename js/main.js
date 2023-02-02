cd6cd86d746601662dc0680b4c8712aaeb74bf6d
Nikita <came95@bk.ru>
2023-02-02 13:23:48 +0300

    for Roman

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const swiper = new Swiper(".js-slider-materials", {
+  speed: 600,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
